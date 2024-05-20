fa26fe54e2d28fee0444c65a7b7e676314ba95a4
ljj123ll <2915144166@qq.com>
2024-05-21 00:52:21 +0800

    测试中文乱码

--- src/main/java/com/example/djproject1/controller/HelloController.java
+++ src/main/java/com/example/djproject1/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(String nickname, String phone){
         System.out.println(phone);
-        return "Love the 1111122222333334444" +nickname;
+        return "Love the 111112222233333444455555" +nickname;
     }
 
 }
