94076effdbd7b94723f53b67d1bc2252a2ae7585
ljj123ll <2915144166@qq.com>
2024-05-21 01:08:15 +0800

    解决不了中文乱码

--- src/main/java/com/example/djproject1/controller/HelloController.java
+++ src/main/java/com/example/djproject1/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     public String hello(String nickname, String phone){
         System.out.println(phone);
         System.out.println("你好世界");
-        return "Love the 111112222233333444455555667777" +nickname;
+        return "Love the 1111122222333334444555556677778" +nickname;
     }
 
 }
