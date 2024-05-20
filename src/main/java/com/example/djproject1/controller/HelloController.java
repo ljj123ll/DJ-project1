bfa3bf645b2e3b5bec432749399633c82c8c9da1
ljj123ll <2915144166@qq.com>
2024-05-21 00:45:33 +0800

    添加4444

--- src/main/java/com/example/djproject1/controller/HelloController.java
+++ src/main/java/com/example/djproject1/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(String nickname, String phone){
         System.out.println(phone);
-        return "Love the 111112222233333" +nickname;
+        return "Love the 1111122222333334444" +nickname;
     }
 
 }
